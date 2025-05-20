7a1fdb56a1ef53fa99692bf242c718123439c4ae
Ubuntu <ubuntu@vps-95f47936.vps.ovh.net.novalocal>
2025-05-20 13:01:44 +0000

    7.1

--- DependencyInjection/AldafluxOvhExtension.php
+++ DependencyInjection/AldafluxOvhExtension.php
@@ -5,7 +5,8 @@ namespace Aldaflux\AldafluxOvhBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
 use Symfony\Component\Config\FileLocator;
 
 class AldafluxOvhExtension extends Extension
